fab26a3cd0ddc6cebf5f23e773fe5280c53f1cfe
Kayanski <kowalski.kowalskin@gmail.com>
2024-12-18 15:48:02 +0100

    More wallets

--- components/wallet.tsx
+++ components/wallet.tsx
@@ -7,7 +7,6 @@ import { useEffect } from "react";
 import Image from 'next/image'
 
 export function WalletModal({ closeModal }) {
-    const { suggestAndConnect: connect } = useSuggestChainAndConnect()
 
     const { data: account } = useAccount()
 
@@ -19,11 +18,19 @@ export function WalletModal({ closeModal }) {
 
     return (<>
         Connect your Wallet :
-        <div style={{ display: "flex", flexDirection: "row", alignItems: "middle" }}>
-            <button onClick={() => connect({ chainInfo: ACTIVE_NETWORK.chain, walletType: WalletType.KEPLR })}><Image src="/keplr-wallet.png" width={40} height={40} alt="Keplr Wallet"></Image></button>
-
+        <div style={{ display: "flex", flexDirection: "row", alignItems: "middle", margin: "20px", gap: "10px", flexWrap: "wrap" }}>
+            <WalletButton walletType={WalletType.KEPLR} img="/keplr-wallet.png" alt="Keplr Wallet" />
+            <WalletButton walletType={WalletType.KEPLR} img="/keplr-wallet.png" alt="Keplr Wallet" />
+            <WalletButton walletType={WalletType.KEPLR} img="/keplr-wallet.png" alt="Keplr Wallet" />
+            <WalletButton walletType={WalletType.KEPLR} img="/keplr-wallet.png" alt="Keplr Wallet" />
         </div >
     </>
-
     );
+}
+
+export function WalletButton({ walletType, img, alt }: { walletType: WalletType, img: string, alt: string }) {
+    const { suggestAndConnect: connect } = useSuggestChainAndConnect()
+    return (<button style={{
+        flex: "1 0 33%"
+    }} onClick={() => connect({ chainInfo: ACTIVE_NETWORK.chain, walletType })}><Image src={img} width={40} height={40} alt={alt}></Image></button>)
 }
