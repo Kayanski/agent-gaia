8a94f3e7d71b4a20c69a4bac558a06fdfec7375d
Kayanski <kowalski.kowalskin@gmail.com>
2025-01-04 00:00:38 +0100

    Avoid pre-rendering

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,7 @@ import { MAX_MESSAGES_DEFAULT } from "@/actions/gaia/constants";
 import { getGameState } from "@/actions/";
 import { getRecentMessages } from "@/actions/";
 import { Main } from "@/app/home/components/Main";
+export const dynamic = 'force-dynamic';
 
 
 export default async function Page() {
